372058dd53733de6c4e9fc979c48ddcfabd3e0cf
Shamim742 <shamimislam2742@gmail.com>
2023-02-20 14:35:28 +0600

    template string

---
+++ js/template-string.js
@@ -0,0 +1,22 @@
+const person = 'Billal Hossain';
+const person1 = 'Sakib Al Hasan';
+const person2 = 'Nayeem Islam';
+
+const multiLine = 'First line text \n'+
+'second line text \n' +
+'third line text \n' +
+'fourth line text'
+console.log(typeof multiLine)
+
+const newMultiLine = `First Line of text
+second line of text
+third line of text
+forth line of text`
+// console.log(newMultiLine)
+
+const a = 20;
+const b = 12;
+const summary = 'sum of: ' + a + ' and ' + b + ' is ' + (a+b);
+const newSummary = `sum of ${a} and ${b} is: ${a+b}`
+// console.log(newSummary)
+// console.log(summary)
